51c251f89a19f7c2ca256df7375932616e87ae21
Shriram bhat <alekhshriram@gmail.com>
2020-09-30 07:48:15 +0530

    -

---
+++ Visualizer.py
@@ -0,0 +1,6 @@
+from Bio.Graphics import GenomeDiagram
+from reportlab.lib import colors
+from reportlab.lib.units import cm
+from Bio import SeqIO
+
+ecg=seqIO.read(r'/Genomes/Ecoli/NZ_CP027599.1[1..5942969].flat','genbank')
